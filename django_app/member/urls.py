3386aa2e0db26258ac8c44b16ad5ac678f5cba2c
secretRami <secret.ryuan@gmail.com>
2017-03-27 21:43:57 +0900

    login_facebook구현

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+
 from . import views
 
 app_name = "member"
 urlpatterns = [
-    url(r'^login/', views.login_fbv, name='login')
+    url(r'^login/$', views.login_fbv, name='login'),
+    url(r'^login/facebook/$', views.login_facebook, name='login_facebook'),
 ]
