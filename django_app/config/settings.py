ac31e4ec04a0204ce91554e2e04808d3ac49e683
secretRami <secret.ryuan@gmail.com>
2017-03-27 19:15:49 +0900

    첫번째 커밋, 페이스북 로그인을 구현하기 위한 작업 시작

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -76,6 +76,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+
+    'member',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +122,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
