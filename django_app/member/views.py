b50d976f104dd700f5c0a3fb5ec1c32e85795011
msjo91 <msjo91@gmail.com>
2017-03-29 17:13:19 +0900

    Change permission_classes of UserViewSet

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.models import Group
+from rest_framework import permissions
 from rest_framework import viewsets
 
 from .models import MyUser
-from .permission import IsAuthenticatedOrCreate, IsOwnerOrReadOnly
 from .serializer import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticatedOrCreate, IsOwnerOrReadOnly)
+    # permission_classes = (IsAuthenticatedOrCreate, IsOwnerOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
