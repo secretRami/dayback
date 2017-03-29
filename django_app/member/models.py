72d69fb07ec0a1680a688ff19961e284952e6694
msjo91 <msjo91@gmail.com>
2017-03-29 14:53:41 +0900

    Create MyUserManager class (BaseUserManager model)

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('이메일 주소는 필수입니다.')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+        user = self.create_user(
+            email=email,
+            nickname=nickname,
+            password=password,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
