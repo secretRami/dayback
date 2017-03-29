e866dfd5c966ff39fb9aeb325118685da2cfb861
msjo91 <msjo91@gmail.com>
2017-03-29 15:28:26 +0900

    Register MyUser model and UserAdmin form, and unregister Group model

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -7,8 +7,10 @@ Referred to Django Documentation v1.10
 https://docs.djangoproject.com/en/1.10/topics/auth/customizing/
 """
 from django import forms
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
 
 from .models import MyUser
 
@@ -98,3 +100,7 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ()
+
+
+admin.site.register(MyUser, UserAdmin)
+admin.site.unregister(Group, )
