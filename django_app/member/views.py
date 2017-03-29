78eb924d24413324994a34f553a8290e1a63cf9a
msjo91 <msjo91@gmail.com>
2017-03-29 16:19:55 +0900

    Create GroupViewSet

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,11 +1,17 @@
+from django.contrib.auth.models import Group
 from rest_framework import viewsets
 
 from .models import MyUser
 from .permission import IsAuthenticatedOrCreate, IsOwnerOrReadOnly
-from .serializer import UserSerializer
+from .serializer import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticatedOrCreate, IsOwnerOrReadOnly)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
