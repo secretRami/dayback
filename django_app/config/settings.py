d1384e843ec51d4498cdd1d3d17fc4d82ff65e82
msjo91 <msjo91@gmail.com>
2017-03-29 15:36:56 +0900

    Set SOCIAL_AUTH_PIPELINE for email and social logins

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -125,6 +125,19 @@ DATABASES = {
     }
 }
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 AUTH_USER_MODEL = 'member.MyUser'
 
 # Password validation
