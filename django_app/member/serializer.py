d810d7a57f1482c4dac24354aae84f47b92bcf52
msjo91 <msjo91@gmail.com>
2017-03-29 16:06:38 +0900

    Create GroupSerializer class

--- django_app/member/serializer.py
+++ django_app/member/serializer.py
@@ -2,7 +2,7 @@
 Serialize to view user list made by custom user model (MyUser).
 커스텀 유저 모델(MyUser)로 생성된 이용자 목록을 직렬화한다.
 """
-
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 from .models import MyUser
@@ -27,3 +27,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = ('url', 'email', 'nickname', 'password')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """
+    Serialize user Group.
+    이용자 그룹 직렬화.
+    """
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
