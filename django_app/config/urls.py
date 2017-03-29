b3a1b8b4a04d3bc847c797198740482b50513f9a
msjo91 <msjo91@gmail.com>
2017-03-29 15:57:37 +0900

    Add auth url

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^auth/', include('rest_framework_social_oauth2.urls')),
 ]
 
 urlpatterns += static(
